e1ab647d5e50de1324eb8d81552931fcd6125f2f
Iwancof <rock.iwancof@gmail.com>
2021-02-15 01:12:33 +0900

    Make test framework

--- src/main.rs
+++ src/main.rs
@@ -16,16 +16,29 @@ pub fn wadachi_start() -> ! {
 
     println!("Hello world");
 
-    loop {}
+    shutdown(0);
+}
+
+pub fn shutdown(exit_code: u32) -> ! {
+    // valid exit code length is 9
+
+    use qemu_exit::QEMUExit;
+
+    let qemu_exit_handler = qemu_exit::RISCV64::new(0x100000);
+    qemu_exit_handler.exit(exit_code);
 }
 
 #[panic_handler]
 #[no_mangle]
-pub fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+pub fn panic(info: &PanicInfo) -> ! {
+    println!("[!] Kernel Panic!");
+    println!("info: {}", info);
+
+    shutdown(1);
 }
 
 #[no_mangle]
 pub fn abort() -> ! {
+    println!("[!] Kernel Abort!");
     loop {}
 }
