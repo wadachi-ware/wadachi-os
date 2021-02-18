fc353b10a189c938a0c36f026870e141312286e8
Iwancof <rock.iwancof@gmail.com>
2021-02-18 20:35:52 +0900

    Add comment. Fix range

--- src/boot.s
+++ src/boot.s
@@ -7,14 +7,17 @@
 
 _start:
 	# set pmpcfg pmpaddr
-	li		t0, 0b00001111 # Write Read Execute
+	li		t0, 0b00001111 
+	# 7     Lock: false --> memory protection is disable in machine mode
+	# 3..=4 Address Maching: TOR --> Top of range
+	# 2     Read: true
+	# 1     Write: true
+	# 0     Execute: true 
+
 	csrw	pmpcfg0, t0
-	li		t0, 0xffff0000 # 0 ~ 0xffffffff
+	li		t0, 0xffff0000 # 0 ~ 0xffff0000
 	csrw	pmpaddr0, t0
 	
-	li		t0,	0x10000000
-	csrw	sepc, t0
-
 	lla		sp, kernel_stack_end
 	j			machine_start
 
