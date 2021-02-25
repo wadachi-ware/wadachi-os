b1c0858fa62325d024553de230b5776ee6dd22c2
Iwancof <rock.iwancof@gmail.com>
2021-02-25 16:05:43 +0900

    Remove target file

--- src/tests/custom_test/target/debug/deps/custom_test-cb574adffc753be5.d
+++
@@ -1,5 +0,0 @@
-/home/iwancof/WorkSpace/Wadachi-ware/wadachi-os/src/tests/custom_test/target/debug/deps/libcustom_test-cb574adffc753be5.so: src/lib.rs
-
-/home/iwancof/WorkSpace/Wadachi-ware/wadachi-os/src/tests/custom_test/target/debug/deps/custom_test-cb574adffc753be5.d: src/lib.rs
-
-src/lib.rs:
--- src/tests/custom_test/target/debug/libcustom_test.d
+++
@@ -1,1 +0,0 @@
-/home/iwancof/WorkSpace/Wadachi-ware/wadachi-os/src/tests/custom_test/target/debug/libcustom_test.so: /home/iwancof/WorkSpace/Wadachi-ware/wadachi-os/src/tests/custom_test/src/lib.rs build.rs
