74587e1eee16121e72dd3a5bf82de046b9579e67
Iwancof <rock.iwancof@gmail.com>
2021-02-28 18:50:27 +0900

    Fix syscall

--- src/riscv/syscall/sys_write.rs
+++ src/riscv/syscall/sys_write.rs
@@ -1,4 +1,5 @@
 use core::ffi::c_void;
+use cstr_core::{c_char, CStr, CString};
 use custom_test::custom_test;
 
 #[allow(unused)]
@@ -17,28 +18,20 @@ pub fn sys_write(buffer: *const c_void) {
         panic!("call sys_write with null pointer");
     }
 
-    let mut p: *const u8 = buffer as *const u8;
-    loop {
-        let v = unsafe { *p };
-        if v == 0 {
-            break;
-        }
-        p = unsafe { p.offset(1) };
-        print!("{}", v as char);
-    }
-}
+    let text = unsafe { CStr::from_ptr(buffer as *const c_char) };
 
-use cstr_core::{CStr, CString};
+    println!("{}", text.to_str().unwrap());
+}
 
 #[no_mangle]
 #[custom_test(ModeSupervisor)]
 fn write_systemcall_test() {
-    let obj = CString::new("Hello").expect("fail");
+    let obj = CString::new("syscall_test_text").expect("fail");
     let test = obj.as_ptr();
     #[cfg(test)]
     assert_eq!(unsafe { WRITE_SYSCALL_FLAG }, false);
     unsafe {
-        llvm_asm!("mv   a1, $0":: "r"(test as usize) :: " volatile");
+        llvm_asm!("mv   a1, a0":: "r"(test as usize) :: " volatile");
         llvm_asm!("li   a0, 0");
         llvm_asm!("ecall");
     }
